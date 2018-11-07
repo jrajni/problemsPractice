230a92252d7ed78c9558ca6a2230ee6ce1236486
jrajni <jrajni6@gmail.com>
2018-11-07 23:55:45 +0530

    Travelling Salesman Problem

--- sumOfRange.cpp/sumOfRange.cpp/main.cpp
+++ sumOfRange.cpp/sumOfRange.cpp/main.cpp
@@ -4,22 +4,29 @@ int main(void)
 {
     int n,q;
     cin>>n>>q;
-    int a[n],l[q],m[q],r[q];
+    long int a[n],l[q],m[q],r[q];
     
-   for(int o=0;o<n;o++){
-       
        for(int i=0;i<n;i++){cin>>a[i];}
-    for(int i,j=0;i,j<n;i,j++){
+  
+    for(int j=0;j<n;j++){
+        
         cin>>m[j]>>l[j]>>r[j];
-       
-        if(m[j]==1)
-        {a[i]++;}
-        cout<<a[i];
     }
-    
-   }
-    
-  // for(int j=0;j<q;j++)cout<<a[j];
-    
+    for(int j=0;j<n;j++){
+        int i=l[j];
+        if(m[j]==1)
+        {
+            a[i]++;}
+        
+        if(m[j]==2)
+        {int s=0;
+            for(int i=0;i<=r[j];i++){s=s+a[i];
+                
+            }cout<<s<<endl;
+        }
+        for(int i=0;i<n;i++)cout<<a[i]<<" ";
+    }
     return 0;
 }
+
+
